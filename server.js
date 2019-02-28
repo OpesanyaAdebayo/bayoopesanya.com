db4c489d62b3aac5fdc4207253013a2096593418
Adebayo Opesanya <bayoopesanya@gmail.com>
2019-02-28 14:58:15 +0100

    added server config

---
+++ server.js
@@ -0,0 +1,12 @@
+ //Install express server
+ const express = require('express');
+ // const path = require('path');
+ const app = express();
+ 
+ app.use(express.static(__dirname + '/dist'));
+ 
+ app.get('/*', (req, res) => {
+     res.sendFile(__dirname + '/dist/index.html');
+ });
+ 
+ app.listen(process.env.PORT || 7004);
