aa5602a8077b98dde141a55fc01ae1a949f5edd1
Adebayo Opesanya <bayoopesanya@gmail.com>
2019-03-09 13:43:27 +0100

    added components for GitHub gists

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { GistComponent } from './gist/gist.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    GistComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/gist/gist.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, Input, ViewChild, ElementRef, AfterViewInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-gist',
+  templateUrl: './gist.component.html',
+  styleUrls: ['./gist.component.css']
+})
+export class GistComponent implements AfterViewInit {
+
+  @ViewChild('iframe') iframe: ElementRef;
+  @Input() gistId;
+  @Input() file: string;
+
+  ngAfterViewInit() {
+    const fileName = (this.file) ? this.file : '';
+    this.iframe.nativeElement.id = 'gist-' + this.gistId;
+    const doc = this.iframe.nativeElement.contentDocument || this.iframe.nativeElement.contentElement.contentWindow;
+    const content = `
+        <html>
+        <head>
+          <base target="_parent">
+        </head>
+        <body onload="parent.document.getElementById('${this.iframe.nativeElement.id}')">
+        <script type="text/javascript" src="https://gist.github.com/${this.gistId}.js?file=${fileName}"></script>
+        </body>
+      </html>
+    `;
+    doc.open();
+    doc.write(content);
+    doc.close();
+  }
+}
